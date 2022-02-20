7efcb64bb91fb0da5fd3d72df4c6a724c2c8862d
Karl Gillott <KarlGillott@googleMail.com>
2022-02-20 07:24:17 +0000

    Clone and pack WIP

---
+++ eWolfCloneAndPack/Clone/CloneFolder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eWolfCloneAndPack.Clone
+{
+    internal class CloneFolder
+    {
+        public CloneFolder(string folder, string name)
+        {
+            Name = name;
+            Folder = folder;    
+        }
+
+        internal void Clone()
+        {
+            //
+        }
+
+        public string Name { get; set; }
+        public string Folder { get; set; }
+
+    }
+}
---
+++ eWolfCloneAndPack/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using eWolfCloneAndPack.Clone;
+
+Console.WriteLine("Hello, World!");
+
+var cf = new CloneFolder(@"C:\Unity3d", "Sci-Fi_Rooms_Pack1_eWolf");
+
+cf.Clone();
+
